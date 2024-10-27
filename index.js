fba9e040125d4b212ef28475cab375c46cfdba38
Adam Driggers <awdriggs@gmail.com>
2024-10-27 13:56:02 +0100

    code working

--- index.js
+++ index.js
@@ -20,19 +20,46 @@ server.listen(port, () => {
 });
 
 
+
+
+let colors = [];
+
 //routes
 io.on('connection', (socket) => {
+
   console.log('We have a new client: ' + socket.id);
 
   socket.on('disconnect', () => {
-    console.log('A client disconnected', socket.id);
+    let indexOfSocket = colors.findIndex(item => item.id === socket.id);
+
+    console.log('A client disconnected', socket.id, indexOfSocket);
+    //clear my space in the colors obj
+    //call splice
+    if (indexOfSocket !== -1) {
+      // Remove the item at the found index
+      colors.splice(indexOfSocket, 1);
+    }
   });
 
-  socket.on('data', (data) => {
-    console.log(data);
+  console.log(socket.id);
+
+  socket.on('color', (color) => {
+    // console.log(socket.id, color);
+
+    //using the socket id, find the index of the id key in the array
+    //if the id already exists in the array, update it, else make a new array item
+    let indexOfSocket = colors.findIndex(item => item.id === socket.id);
+
+    if(indexOfSocket == -1){ //socket info doesn't exist yet
+      colors.push({id: socket.id, color: color});
+    } else {
+      colors[indexOfSocket].color = color; //update existing color
+    }
+
+    // console.log(colors);
 
     //send to all clients, including us
-    io.emit('data', data);
+    io.emit('color', colors); //send the array of colors
 
     //send to all clients, except us
     //socket.broadcast.emit('data', data);
--- public/app.js
+++ public/app.js
@@ -1,32 +1,90 @@
-console.log('The page is working!');
+console.log('Frontend working bro!');
+
+let socket = io(); //import from the socket js file
+
+let capture;
+let ready = false;
 
 //STEP 1. p5 setup
-let c;
 
 function setup(){
   createCanvas(windowWidth, windowHeight);
 
-  background(0, 255, 0);
+  //setup the capture
+  capture = createCapture(VIDEO, {video:{ facingMode:"environment"}}, ()=>{
+    console.log("video ready");
+    ready = true;
+  });
 
-  c = {r: floor(random(0,255)), g: floor(random(0,255)), b: floor(random(0,255))}
+  capture.hide();
+
+  noStroke();
 
 }
 
+function draw() {
+  pushColor();
+}
 
-function mouseMoved(){
+//average
+//vars for the r g and b running count
+//loop through every pixel in the camera array
+//r + = this pixels r value, same for g and b
+//divide r value by the number of pixel, floor it
+//repeat for other channels
+//return the the color value.
+function averageColor(){
+  capture.loadPixels();
+  // console.log(capture.pixels.length);
+  let r = g = b = 0; //weird, but init all variable to 0, chatgpt
+
+  for (let i = 0; i < capture.pixels.length; i += 4) {
+    r += capture.pixels[i];     // Red value
+    g += capture.pixels[i + 1]; // Green value
+    b += capture.pixels[i + 2]; // Blue value
+  }
+
+  let numPixels = capture.pixels.length/4;
+  // console.log(r, g, b);
+  let color = {r: floor(r/numPixels), g: floor(g/numPixels), b: floor(b/numPixels)};
+  return color 
+}
+
+function pushColor(){
+  //push your average color to the server
+  if(ready){
+    let color = averageColor(); //change the the average color
+    socket.emit('color', color);   
+  }
+}
+
+// function mouseMoved(){
 
   // ellipse(mouseX, mouseY, 20, 20);
-  socket.emit('data', {c: c, x: mouseX, y: mouseY});
-}
+  // socket.emit('data', {c: c, x: mouseX, y: mouseY});
+// }
 
-let socket = io();
 
 socket.on("connect", ()=> {
   console.log("socks are on", socket.id);
 });
 
-socket.on('data', (data) => {
+socket.on('color', (colors) => {
   // debugger;
-  fill(data.c.r, data.c.g, data.c.b);
-  ellipse(data.x, data.y, 20, 20);
+  // fill(color.r, color.g, color.b);
+  // rect(100, 100, 100, 100); //for testing
+  //console.log(colors.length);
+  let h = height/colors.length + 1;
+  for(let i = 0; i < colors.length; i++){
+    let y = i * h;
+    let c = color(colors[i].color.r, colors[i].color.g, colors[i].color.b)
+    fill(c);
+    rect(0, y, width, h);
+
+    if(socket.id == colors[i].id){
+      fill(0);
+      text("me", 10, y + 10);
+    }
+  }
+
 });
